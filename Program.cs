b9ac7fbeed81af07106aee74ab9faa37365fc727
Deviationlark <zilamovpetar@gmail.com>
2024-02-18 14:52:26 +0100

    Fixed DivisionGame and Added History function

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Globalization;
+List<string> games = new List<string>();
 bool gameEnd = false;
 string? result;
-int points = 0;
 int numQuestions = 0;
 while (!gameEnd)
 {
@@ -13,7 +13,7 @@ void ShowMenu()
 {
     Console.Clear();
     Console.WriteLine("MATH GAME");
-    Console.WriteLine("----------\n");
+    Console.WriteLine("----------");
     Console.WriteLine("1. Play");
     Console.WriteLine("2. History");
     Console.WriteLine("3. Exit");
@@ -23,9 +23,10 @@ void ShowMenu()
     switch (result)
     {
         case "1":
-        ChooseGame();
+            ChooseGame();
             break;
         case "2":
+            ShowHistory();
             break;
         case "3":
             Console.WriteLine("BYE");
@@ -38,11 +39,11 @@ void ShowMenu()
     }
 }
 
-void ChooseGame() 
+void ChooseGame()
 {
     Console.Clear();
     Console.WriteLine("MATH GAME");
-    Console.WriteLine("----------\n");
+    Console.WriteLine("----------");
     Console.WriteLine("Choose a game: ");
     Console.WriteLine("1. Addition");
     Console.WriteLine("2. Subtraction");
@@ -50,19 +51,55 @@ void ChooseGame()
     Console.WriteLine("4. Divison");
     result = Console.ReadLine();
 
-    switch(result)
+    switch (result)
     {
         case "1":
-            AdditionGame();
+            try
+            {
+                AdditionGame();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Press enter to go back to main menu");
+                Console.ReadLine();
+            }
             break;
         case "2":
-            SubtractionGame();
+            try
+            {
+                SubtractionGame();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Press enter to go back to main menu");
+                Console.ReadLine();
+            }
             break;
         case "3":
-            MultiplicationGame();
+            try
+            {
+                MultiplicationGame();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Press enter to go back to main menu");
+                Console.ReadLine();
+            }
             break;
         case "4":
-            DivisionGame();
+            try
+            {
+                DivisionGame();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Press enter to go back to main menu");
+                Console.ReadLine();
+            }
             break;
         default:
             Console.WriteLine("Thats not a valid option, please type only the number of the option.");
@@ -77,8 +114,9 @@ void AdditionGame()
     var random = new Random();
     int number1;
     int number2;
-    
-    for(int i = 0; i < numQuestions; i++)
+    int points = 0;
+
+    for (int i = 0; i < numQuestions; i++)
     {
         Console.Clear();
         number1 = random.Next(1, 10);
@@ -103,6 +141,7 @@ void AdditionGame()
 
     Console.WriteLine($"The End. You scored {points} points. Press enter to go back to main menu.");
     Console.ReadLine();
+    AddToHistory("Addition", points);
     ShowMenu();
 }
 
@@ -112,8 +151,9 @@ void SubtractionGame()
     var random = new Random();
     int number1;
     int number2;
-    
-    for(int i = 0; i < numQuestions; i++)
+    int points = 0;
+
+    for (int i = 0; i < numQuestions; i++)
     {
         Console.Clear();
         number1 = random.Next(1, 10);
@@ -138,6 +178,7 @@ void SubtractionGame()
 
     Console.WriteLine($"The End. You scored {points} points. Press enter to go back to main menu.");
     Console.ReadLine();
+    AddToHistory("Subtraction", points);
     ShowMenu();
 }
 
@@ -147,8 +188,9 @@ void MultiplicationGame()
     var random = new Random();
     int number1;
     int number2;
-    
-    for(int i = 0; i < numQuestions; i++)
+    int points = 0;
+
+    for (int i = 0; i < numQuestions; i++)
     {
         Console.Clear();
         number1 = random.Next(1, 10);
@@ -173,27 +215,27 @@ void MultiplicationGame()
 
     Console.WriteLine($"The End. You scored {points} points. Press enter to go back to main menu.");
     Console.ReadLine();
+    AddToHistory("Multiplication", points);
     ShowMenu();
 }
 
 void DivisionGame()
 {
-    int[] Numbers = DivisionNumbers();
-
     numQuestions = QuestionsCount();
     var random = new Random();
-    int number1 = Numbers[0];
-    int number2 = Numbers[1];
-    
-    for(int i = 0; i < numQuestions; i++)
+    int points = 0;
+    for (int i = 0; i < numQuestions; i++)
     {
+        int[] Numbers = DivisionNumbers();
+        int number1 = Numbers[0];
+        int number2 = Numbers[1];
         Console.Clear();
 
         Console.WriteLine($"Division Game: {points} points");
         Console.WriteLine($"{number1} / {number2} = ");
         result = Console.ReadLine();
 
-        if (Convert.ToInt32(result) == (number1 * number2))
+        if (Convert.ToInt32(result) == (number1 / number2))
         {
             Console.WriteLine("Correct! + 1 point");
             Console.ReadLine();
@@ -208,6 +250,7 @@ void DivisionGame()
 
     Console.WriteLine($"The End. You scored {points} points. Press enter to go back to main menu.");
     Console.ReadLine();
+    AddToHistory("Division", points);
     ShowMenu();
 
 }
@@ -216,23 +259,43 @@ int QuestionsCount()
 {
     Console.Clear();
     Console.WriteLine("MATH GAME");
-    Console.WriteLine("----------\n");
+    Console.WriteLine("----------");
     int num;
     Console.WriteLine("Enter the amount of questions you want to answer: ");
     result = Console.ReadLine();
     num = Convert.ToInt32(result);
     return num;
 }
-// Fix this
+
 int[] DivisionNumbers()
 {
     var random = new Random();
     int[] Numbers = new int[2];
     Numbers[0] = random.Next(1, 100);
     Numbers[1] = random.Next(1, 100);
-    
-    if(Numbers[0] % Numbers[1] != 0)
-        DivisionNumbers();
 
+    while (Numbers[0] % Numbers[1] != 0)
+    {
+        Numbers[0] = random.Next(1, 100);
+        Numbers[1] = random.Next(1, 100);
+    }
     return Numbers;
+}
+
+void AddToHistory(string gamemode, int points)
+{
+    games.Add($"{gamemode}: {points}pts");
+}
+
+void ShowHistory()
+{
+    Console.Clear();
+    Console.WriteLine("MATH GAME");
+    Console.WriteLine("----------\n");
+    Console.WriteLine("HISTORY");
+    foreach (string game in games)
+    {
+        Console.WriteLine(game);
+    }
+    Console.ReadLine();
 }
--- obj/Debug/net8.0/Math Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/Math Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Math Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e235c14245ed157a6e853bad70016e4455058a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18a13b6f09cf66cbff0cc3f12bc80ab903532952")]
 [assembly: System.Reflection.AssemblyProductAttribute("Math Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Math Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
